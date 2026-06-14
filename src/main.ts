02d7ec3b1ec31a6051bdaa23d90756e1dea2e65b
Joel-Tito <jtito.alexander@gmail.com>
2026-06-14 04:45:05 -0500

    fix: add production CORS for Vercel frontend

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://crud-vite-nest.vercel.app'
+    ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   });
 
